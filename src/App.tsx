cfc26dd447bcd44187bf65752aef7031abfbe74f
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-09-30 21:53:07 -0300

    feature: cria estrutura HTML do componente Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import  Comment  from "./components/Comment";
+import NewComment from "./components/NewComment";
+import Modal from "./components/Modal";
 
 function App() {
   return (
     <div className="App">
       <Comment />
+      <NewComment picture="" altText="" buttonText="Reply" placeholderText="Add a comment..."/>
+      <Modal />
     </div>
   );
 }
--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -4,7 +4,6 @@ import iconeEdit from "../images/icon-edit.svg";
 
 import UserInfo from "./UserInfo";
 import { useState } from "react";
-import NewComment from "./NewComment";
 
 export default function Comment()  {
     const [isAResponseComment, setisAResponseComment ] = useState<boolean>(false);
@@ -45,7 +44,6 @@ export default function Comment()  {
                 { isAResponseComment && <a href="#reply"className="comment__body-repliedTo">@usernameReplyTo</a> }
                 <p className="comment__paragraph"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta quaerat incidunt necessitatibus quasi odit, consequuntur excepturi. Aliquid, repudiandae ipsum? Quas aperiam sint inventore soluta impedit dignissimos velit minima quasi veritatis.</p>
             </div>
-            <NewComment picture="" altText="" buttonText="Reply" placeholderText="Add a comment..."/>
         </div>
     )
 }
