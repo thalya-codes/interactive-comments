e228494eefc37061dd279e846a6aeb602a8be513
Thalya Stéffany <thalyacoiimbra@gmail.com>
2022-12-26 16:35:33 -0300

    fix: realiza ajuste na estrutura do componente Comment para que as replies seja renderizadas corretamente.

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,84 +1,106 @@
-import './style.scss';
-import HeaderComment from 'components/HeaderComment/HeaderComment';
-import { ICommentsData, IProps } from 'interfaces/IComment';
-import { useState } from 'react';
+import "./style.scss";
+import HeaderComment from "components/HeaderComment/HeaderComment";
+import { ICommentsData, IProps } from "interfaces/IComment";
+import { useState } from "react";
 
-export default function Comment({ data, id }: IProps)  {
-    const [counter, setCounter] = useState(0);
-  
-    const sumScore = (id: number, isReply: boolean) => {        
-        const comments: ICommentsData[] = data.comments;
-     
-        if (isReply) {
-            comments.forEach((comentario: ICommentsData):void => comentario.replies.forEach((reply: ICommentsData):void => {
-                reply.id === id && setCounter(reply.score++);
-           }));
-           
-        } else {
-            const indexElement: number = data.comments.findIndex((comment: ICommentsData) => comment.id === id);
-            setCounter(comments[indexElement].score++);            
-        }
-           
+export default function Comment({ data, id }: IProps) {
+  const [counter, setCounter] = useState(0);
+
+  const sumScore = (id: number, isReply: boolean) => {
+    const comments: ICommentsData[] = data.comments;
+
+    if (isReply) {
+      comments.forEach((comentario: ICommentsData): void =>
+        comentario.replies.forEach((reply: ICommentsData): void => {
+          reply.id === id && setCounter(reply.score++);
+        })
+      );
+    } else {
+      const indexElement: number = data.comments.findIndex(
+        (comment: ICommentsData) => comment.id === id
+      );
+      setCounter(comments[indexElement].score++);
     }
+  };
 
-    const substractScore = (id: number, isReply: boolean) => {
-        const comments: ICommentsData[] = data.comments;
-        
-        if (isReply) {
-            comments.forEach((comentario: ICommentsData):void => comentario.replies.forEach((reply: ICommentsData):void => {
-                (reply.id === id && reply.score > 0) && setCounter(reply.score--);
-           }));
-           
-        } else {
-            const indexElement: number = data.comments.findIndex((comment: ICommentsData) => comment.id === id);
-            comments[indexElement].score > 0 && setCounter(comments[indexElement].score--);
-        }   
-    }  
+  const substractScore = (id: number, isReply: boolean) => {
+    const comments: ICommentsData[] = data.comments;
 
-    const commentStructure = (data: ICommentsData, isReply: boolean) => {    
-        return (            
-            <div className='d-flex comment__wrapper justify-content-between comment' key={`key-comment-${data.user.username}--${data.id}`}>        
-                <div className='d-flex flex-column align-items-center fw-bold comment__score'>
-                    <button className='comment__score-button fw-bold btn'
-                        onClick={() => sumScore(data.id, isReply)}
-                    >+</button>         
-                    <span className='comment__score-number'> {data.score} </span>        
-                    <button className='comment__score-button fw-bold btn'
-                        onClick={() => substractScore(data.id, isReply)}
-                    >-</button>
-                </div>    
+    if (isReply) {
+      comments.forEach((comentario: ICommentsData): void =>
+        comentario.replies.forEach((reply: ICommentsData): void => {
+          reply.id === id && reply.score > 0 && setCounter(reply.score--);
+        })
+      );
+    } else {
+      const indexElement: number = data.comments.findIndex(
+        (comment: ICommentsData) => comment.id === id
+      );
+      comments[indexElement].score > 0 &&
+        setCounter(comments[indexElement].score--);
+    }
+  };
 
-                <div className='d-flex flex-column comment__header-and-body' >
-                    <HeaderComment
-                        id={`header-comment--${data.id}`}
-                        username={ data.user.username }
-                        picture={require(`../../${ data.user.image.png  }`)}
-                        altText=''
-                        isTheAuthor={ data.user.username === 'juliusomo' && true }
-                        createdAt={ data.createdAt }
-                    />
-                
-                    <div className='mt-3 comment__body'>                  
-                        { isReply && <span className='comment__body-replyingTo'>@{ data.replyingTo}</span> }
-                        <p className='comment__body-paragraph'>{ data.content }</p>
-                    </div>
-                </div>
-            </div>
-        );
-    };
-
+  const commentStructure = (data: ICommentsData, isReply: boolean) => {
     return (
-        <div id={ id } className='container'>
-            <div>
-                { data.comments.map((comment: ICommentsData) =>  commentStructure(comment, false)) }
-            </div>
-
-            <div className='container__replies'>
-                { data.comments.map((comment: ICommentsData) => (
-                    comment.replies.map((reply: ICommentsData) => commentStructure(reply, true)))) 
-                }
-            </div>
+      <div
+        className="d-flex comment__wrapper justify-content-between comment"
+        key={`key-comment-${data.user.username}--${data.id}`}
+      >
+        <div className="d-flex flex-column align-items-center fw-bold comment__score">
+          <button
+            className="comment__score-button fw-bold btn"
+            onClick={() => sumScore(data.id, isReply)}
+          >
+            +
+          </button>
+          <span className="comment__score-number"> {data.score} </span>
+          <button
+            className="comment__score-button fw-bold btn"
+            onClick={() => substractScore(data.id, isReply)}
+          >
+            -
+          </button>
         </div>
 
+        <div className="d-flex flex-column comment__header-and-body">
+          <HeaderComment
+            id={`header-comment--${data.id}`}
+            username={data.user.username}
+            picture={require(`../../${data.user.image.png}`)}
+            altText=""
+            isTheAuthor={data.user.username === "juliusomo" && true}
+            createdAt={data.createdAt}
+          />
+
+          <div className="mt-3 comment__body">
+            {isReply && (
+              <span className="comment__body-replyingTo">
+                @{data.replyingTo}
+              </span>
+            )}
+            <p className="comment__body-paragraph">{data.content}</p>
+          </div>
+        </div>
+      </div>
     );
+  };
+
+  return (
+    <div id={id} className="container">
+      {data.comments.map((comment: ICommentsData) => (
+        <>
+          {commentStructure(comment, false)}
+					
+          {comment.replies.length > 0 && (
+            <div className="container__replies">
+              {comment.replies.map((reply: ICommentsData) =>
+                commentStructure(reply, true)
+              )}
+            </div>
+          )}
+        </>
+      ))}
+    </div>
+  );
 }
