42be93401b80459a3cbc4e6beebf33aef9493e59
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-01-05 20:26:24 -0300

    chore: cria setup inicial do projeto

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'; 
+
+// https://vitejs.dev/config/
+console.log("path.resolve('src', 'components': ", path.resolve('src', 'components'))
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve('src'),
+      '@components': path.resolve('src', 'components'),
+      '@assets': path.resolve('src', 'assets'),
+    },
+  }, 
+  plugins: [react()],
+})
