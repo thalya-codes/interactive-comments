fc8bad673fe8f82fa8958fb863097be7e5069147
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-01-05 21:12:56 -0300

    style: formata todos os arquivos iniciais

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'; 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
-console.log("path.resolve('src', 'components': ", path.resolve('src', 'components'))
+console.log(
+  "path.resolve('src', 'components': ",
+  path.resolve('src', 'components')
+);
 export default defineConfig({
   resolve: {
     alias: {
@@ -11,6 +14,6 @@ export default defineConfig({
       '@components': path.resolve('src', 'components'),
       '@assets': path.resolve('src', 'assets'),
     },
-  }, 
+  },
   plugins: [react()],
-})
+});
