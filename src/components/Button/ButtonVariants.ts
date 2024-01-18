ccb2a4380ddcb397aa1d5cd6c985adc9cf93886c
Thalya <thalyacoiimbra@gmail.com>
2024-01-17 22:20:11 -0300

    feat(Button): cria variações de estilo do componente

---
+++ src/components/Button/ButtonVariants.ts
@@ -0,0 +1,8 @@
+export enum ButtonVariants {
+  'soft-red' = 'text-primary-soft-red font-medium ',
+  'moderate-blue' = 'text-primary-moderate-blue font-medium ',
+  'light-grayish-blue' = 'text-primary-light-grayish-blue font-medium',
+  'filled-moderate-blue' = 'bg-primary-moderate-blue uppercase  text-neutral-white  tracking-[0.5px] rounded-lg py-3 px-6',
+  'filled-soft-red' = 'bg-primary-soft-red uppercase  text-neutral-white  tracking-[0.5px] rounded-lg py-3 px-6',
+  'filled-grayish-blue' = 'bg-neutral-grayish-blue uppercase  text-neutral-white  tracking-[0.5px] rounded-lg py-3 px-6',
+}
