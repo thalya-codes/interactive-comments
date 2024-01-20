a1f4adecf78906dd8403af44ab140a509be1185b
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-01-20 13:38:33 -0300

    refactor(data): altera valor da prop createdAt Altera string por uma 'data string' no padrão ISO

--- src/data/index.ts
+++ src/data/index.ts
@@ -13,7 +13,7 @@ export default {
       id: uuid(),
       content:
         "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      createdAt: '1 month ago',
+      createdAt: new Date('2023/12/15').toISOString(),
       score: 12,
       hasAlreadyVoted: false,
       isEditing: false,
@@ -30,7 +30,7 @@ export default {
       id: uuid(),
       content:
         "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-      createdAt: '2 weeks ago',
+      createdAt: new Date('2024/01/06').toISOString(),
       score: 5,
       hasAlreadyVoted: false,
       isEditing: false,
@@ -46,7 +46,7 @@ export default {
           id: uuid(),
           content:
             "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-          createdAt: '1 week ago',
+          createdAt: new Date('2024/01/13').toISOString(),
           score: 4,
           replyingTo: 'maxblagun',
           isEditing: false,
@@ -62,7 +62,7 @@ export default {
           id: uuid(),
           content:
             "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-          createdAt: '2 days ago',
+          createdAt: new Date('2024/01/18').toISOString(),
           score: 2,
           isDeleting: false,
           isEditing: false,
