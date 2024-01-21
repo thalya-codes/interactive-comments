db29a1cbc23914df89f9178db5b169c13124b121
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-01-21 20:12:37 -0300

    chore(tailwind.config): extende cores e font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,26 @@
 export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
-    colors: {
-      primary: {
-        'moderate-blue': 'hsl(238, 40%, 52%)',
-        'soft-red': 'hsl(358, 79%, 66%)',
-        'light-grayish-blue': 'hsl(239, 57%, 85%)',
-        'extra-light-grayish-blue': 'hsl(240, 3%, 98%)',
-        'pale-red': 'hsl(357, 100%, 86%)',
+    extend: {
+      colors: {
+        primary: {
+          'moderate-blue': 'hsl(238, 40%, 52%)',
+          'soft-red': 'hsl(358, 79%, 66%)',
+          'light-grayish-blue': 'hsl(239, 57%, 85%)',
+          'extra-light-grayish-blue': 'hsl(240, 3%, 98%)',
+          'pale-red': 'hsl(357, 100%, 86%)',
+        },
+        neutral: {
+          'dark-blue': 'hsl(212, 24%, 26%)',
+          'grayish-blue': 'hsl(211, 10%, 45%)',
+          'light-gray': 'hsl(223, 19%, 93%)',
+          'very-light-gray': 'hsl(228, 33%, 97%)',
+          white: 'hsl(0, 0%, 100%)',
+        },
       },
-      neutral: {
-        'dark-blue': 'hsl(212, 24%, 26%)',
-        'grayish-blue': 'hsl(211, 10%, 45%)',
-        'light-gray': 'hsl(223, 19%, 93%)',
-        'very-light-gray': 'hsl(228, 33%, 97%)',
-        'white': 'hsl(0, 0%, 100%)',
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
       },
-    },
-    fontFamily: {
-      rubik: ['Rubik', 'sans-serif'],
     },
   },
   plugins: [],
