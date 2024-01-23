b18a989adea2b3de095f2988429f411a7741487c
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-01-23 20:44:33 -0300

    feat(IComment): adiciona novas props

--- src/interfaces/IComment.ts
+++ src/interfaces/IComment.ts
@@ -1,8 +1,12 @@
 import { IHeader } from '@/interfaces/IHeader';
 import { IVoteControls } from '@/interfaces/IVoteControls';
+import { SetStateAction, Dispatch } from 'react';
 
 export interface IComment
   extends Omit<IHeader, 'children'>,
     IVoteControls {
   content: string;
+  setIsReplyingMode?: Dispatch<SetStateAction<boolean>>;
+  setIsEditingMode?: Dispatch<SetStateAction<boolean>>;
+  setShowModal?: Dispatch<SetStateAction<boolean>>;
 }
