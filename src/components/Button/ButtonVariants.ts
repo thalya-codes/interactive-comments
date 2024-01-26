7b9ae255b22ffb8aecb252484c8433a4cd8d2a2f
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-01-26 17:50:00 -0300

    fix(ButtonVariants): realiza ajuste na estilização

--- src/components/Button/ButtonVariants.ts
+++ src/components/Button/ButtonVariants.ts
@@ -2,7 +2,7 @@ export enum ButtonVariants {
   'soft-red' = 'text-primary-soft-red font-medium ',
   'moderate-blue' = 'text-primary-moderate-blue font-medium ',
   'light-grayish-blue' = 'text-primary-light-grayish-blue font-medium',
-  'filled-moderate-blue' = 'bg-primary-moderate-blue uppercase  text-neutral-white  tracking-[0.5px] rounded-lg py-3 px-6',
-  'filled-soft-red' = 'bg-primary-soft-red uppercase  text-neutral-white  tracking-[0.5px] rounded-lg py-3 px-6',
-  'filled-grayish-blue' = 'bg-neutral-grayish-blue uppercase  text-neutral-white  tracking-[0.5px] rounded-lg py-3 px-6',
+  'filled-moderate-blue' = 'bg-primary-moderate-blue uppercase text-neutral-white  tracking-[0.5px] rounded-lg py-[0.55rem] px-6',
+  'filled-soft-red' = 'bg-primary-soft-red uppercase text-sm text-neutral-white  tracking-[0.5px] rounded-lg py-[0.55rem] px-6',
+  'filled-grayish-blue' = 'bg-neutral-grayish-blue uppercase text-sm text-neutral-white  tracking-[0.5px] rounded-lg py-3 px-6',
 }
