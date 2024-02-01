60e9687c1d3a8b7e9aa08557a82b71ac3737e057
Thalya <thalyacoiimbra@gmail.com>
2024-02-01 19:29:36 -0300

    fix(useModal): adiciona/remove overflow do body

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,18 +1,16 @@
-import { RefObject, useState } from 'react';
+import { useState } from 'react';
 
-export function useModal(
-  listRef: RefObject<HTMLUListElement>
-) {
+export function useModal() {
   const [showModal, setShowModal] = useState(false);
 
   const onShowModal = (): void => {
     setShowModal(true);
-    listRef.current?.classList.add('overflow-hidden');
+   document.body.classList.add('overflow-hidden');
   };
 
   const onHideModal = (): void => {
     setShowModal(false);
-    listRef.current?.classList.remove('overflow-hidden');
+   document.body.classList.remove('overflow-hidden');
   };
 
   return {
