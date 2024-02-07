06b839dded40559b53f169467ed2df9d6e3e29e1
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-07 08:05:02 -0300

    feat(VoteControls): trata responsividade

--- src/components/VoteControls/index.tsx
+++ src/components/VoteControls/index.tsx
@@ -13,11 +13,11 @@ export function VoteControls({
 
   return (
     <div
-      className={`flex flex-col justify-center items-center w-10 h-20 px-2 box-border bg-primary-extra-light-grayish-blue shadow-md rounded-md font-medium ${className}`}
+      className={`flex flex-col justify-center items-center w-10 h-20 px-2 box-border bg-primary-extra-light-grayish-blue shadow-md rounded-md font-medium sm:flex-row sm:w-24 sm:h-10 sm:gap-5 ${className}`}
     >
       <Button
         variants='light-grayish-blue'
-        className='hover:text-primary-moderate-blue'
+        className='hover:text-primary-moderate-blue sm:text-lg'
         onClick={() => dispatch(addVote({ id, parentId }))}
         aria-label='Add vote'
       >
@@ -30,7 +30,7 @@ export function VoteControls({
 
       <Button
         variants='light-grayish-blue'
-        className='hover:text-primary-moderate-blue'
+        className='hover:text-primary-moderate-blue sm:text-lg'
         onClick={() =>
           dispatch(removeVote({ id, parentId }))
         }
