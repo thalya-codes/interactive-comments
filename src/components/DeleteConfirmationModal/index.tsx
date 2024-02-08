219b58e54cc6dfd1ef43cb13662cb75ce99060da
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-08 20:27:30 -0300

    feat(DeleteConfirmationModal): trata width em telas 'xs'

--- src/components/DeleteConfirmationModal/index.tsx
+++ src/components/DeleteConfirmationModal/index.tsx
@@ -17,7 +17,7 @@ export function DeleteConfirmationModal({
 
   return (
     <div className='fixed h-screen w-screen flex justify-center items-center bg-neutral-dark-blue-opacity-04 top-0 left-0'>
-      <div className='flex flex-col gap-3 w-[380px] bg-white p-4 rounded-md relative z-20'>
+      <div className='flex flex-col gap-3 w-96 xs:w-[310px] bg-white p-4 rounded-md relative z-20'>
         <h2 className='font-bold text-lg text-neutral-dark-blue'>
           Delete comment
         </h2>
@@ -27,7 +27,7 @@ export function DeleteConfirmationModal({
           will remove the comment and can't be undone.
         </p>
 
-        <div className='flex justify-between'>
+        <div className='flex justify-between xs:flex-col xs:gap-4'>
           <Button
             variants='filled-grayish-blue'
             onClick={onHideModal}
@@ -37,6 +37,7 @@ export function DeleteConfirmationModal({
 
           <Button
             variants='filled-soft-red'
+            className='xs:h-11'
             onClick={handleDeleteComment}
           >
             Yes, delete
