ec8e27abfdea78eaa2f07208943682f86b46e1c8
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-09 17:52:11 -0300

    refactor(VoteControls): substitui símbolos por ícones

--- src/components/VoteControls/index.tsx
+++ src/components/VoteControls/index.tsx
@@ -2,6 +2,8 @@ import { useDispatch } from 'react-redux';
 import { Button } from '../Button';
 import { addVote, removeVote } from '@/store/slice';
 import { IVoteControls } from '@/interfaces/IVoteControls';
+import { MinusIcon } from '../Icons/MinusIcon';
+import { PlusIcon } from '../Icons/PlusIcon';
 
 export function VoteControls({
   id,
@@ -13,15 +15,15 @@ export function VoteControls({
 
   return (
     <div
-      className={`flex flex-col justify-center items-center w-10 h-20 px-2 box-border bg-primary-extra-light-grayish-blue shadow-md rounded-md font-medium sm:flex-row sm:w-24 sm:h-10 sm:gap-5 ${className}`}
+      className={`flex flex-col justify-evenly items-center w-10 h-20 px-2 box-border bg-primary-extra-light-grayish-blue shadow-md rounded-md font-medium sm:flex-row sm:w-24 sm:h-10 sm:gap-5 ${className}`}
     >
       <Button
         variants='light-grayish-blue'
-        className='hover:text-primary-moderate-blue sm:text-lg'
+        className='group hover:text-primary-moderate-blue sm:text-lg'
         onClick={() => dispatch(addVote({ id, parentId }))}
         aria-label='Add vote'
       >
-        +
+        <PlusIcon />
       </Button>
 
       <span className='text-primary-moderate-blue'>
@@ -30,13 +32,13 @@ export function VoteControls({
 
       <Button
         variants='light-grayish-blue'
-        className='hover:text-primary-moderate-blue sm:text-lg'
+        className='group hover:text-primary-moderate-blue sm:text-lg'
         onClick={() =>
           dispatch(removeVote({ id, parentId }))
         }
         aria-label='Remove vote'
       >
-        -
+        <MinusIcon />
       </Button>
     </div>
   );
