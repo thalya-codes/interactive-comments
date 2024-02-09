8b6a15f19ded7ba8938f2a770adf169fa1dc99cd
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-09 17:52:53 -0300

    feat(CommentRightButtons): adiciona ícones

--- src/components/CommentRightButtons.tsx
+++ src/components/CommentRightButtons.tsx
@@ -1,34 +1,46 @@
-import { ICommentRightButtons } from "@/interfaces/ICommentRightButtons";
-import { Button } from "./Button";
+import { ICommentRightButtons } from '@/interfaces/ICommentRightButtons';
+import { Button } from './Button';
+import { ReplyIcon } from './Icons/ReplyIcon';
+import { DeleteIcon } from './Icons/DeleteIcon';
+import { EditIcon } from './Icons/EditIcon';
 
 export function CommentRightButtons({
   className,
   isAuthor,
   onShowModal,
   handleEditingMode,
-  handleReplyingMode
+  handleReplyingMode,
 }: ICommentRightButtons) {
+  const buttonWithIconClass = 'flex gap-2 items-center';
+
   return isAuthor ? (
-    <div className={`flex gap-2  ${className}`}>
-      <Button variants='soft-red' onClick={onShowModal}>
-        Delete
+    <div className={`flex gap-5  ${className}`}>
+      <Button
+        variants='soft-red'
+        className={buttonWithIconClass}
+        onClick={onShowModal}
+      >
+        <DeleteIcon />
+        <span>Delete</span>
       </Button>
 
       <Button
         variants='moderate-blue'
+        className={`${className} ${buttonWithIconClass}`}
         onClick={handleEditingMode}
-        className={className}
       >
-        Edit
+        <EditIcon />
+        <span>Edit</span>
       </Button>
     </div>
   ) : (
     <Button
       variants='moderate-blue'
-      className={className}
+      className={`${className} ${buttonWithIconClass}`}
       onClick={handleReplyingMode}
     >
-      Reply
+      <ReplyIcon />
+      <span>Reply</span>
     </Button>
   );
 }
