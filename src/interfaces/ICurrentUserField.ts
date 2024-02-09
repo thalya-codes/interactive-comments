b4b948501816bbe087f0fd5beffe8377fcac2bd0
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-08 21:02:01 -0300

    feat(ICurrentUserField): adiciona novas props

--- src/interfaces/ICurrentUserField.ts
+++ src/interfaces/ICurrentUserField.ts
@@ -4,5 +4,7 @@ export interface ICurrentUserField {
   value: string;
   setValue: Dispatch<SetStateAction<string>>;
   buttonText: string;
+  className?: string;
+  placeholder?: string;
   onClick: () => void;
 }
