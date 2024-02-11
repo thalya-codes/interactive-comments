390c649a51d769ff6b00dfdf2578d42c5155c2b4
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-11 13:48:42 -0300

    refactor(IComment): altera tipagem e nome de props

--- src/interfaces/IComment.ts
+++ src/interfaces/IComment.ts
@@ -1,12 +1,18 @@
 import { IHeader } from '@/interfaces/IHeader';
 import { IVoteControls } from '@/interfaces/IVoteControls';
 import { SetStateAction, Dispatch, ReactNode } from 'react';
+import {
+  IEditingMode,
+  IReplyingMode,
+} from './ICommentContainer';
 
 export interface IComment
   extends Omit<IHeader, 'children'>,
     IVoteControls {
   children: ReactNode;
-  setIsReplyingMode: Dispatch<SetStateAction<boolean>>;
-  setIsEditingMode: Dispatch<SetStateAction<boolean>>;
+  setReplyingMode: Dispatch<
+    SetStateAction<IReplyingMode>
+  >;
+  setEditingMode: Dispatch<SetStateAction<IEditingMode>>;
   onShowModal: () => void;
 }
