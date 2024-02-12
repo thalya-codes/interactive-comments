b7c2705d90652e579f80be410b3698a096db5fea
Thalya <thalyacoiimbra@gmail.com>
2024-02-12 16:30:33 -0300

    feat(store/index): persiste dados

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore  } from '@reduxjs/toolkit';
 import { reducer } from './slice';
+import { loadComments, persistComments } from './localStorage';
 
 export default configureStore({
   reducer: { comments: reducer },
+  preloadedState: loadComments(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(persistComments),
 });
