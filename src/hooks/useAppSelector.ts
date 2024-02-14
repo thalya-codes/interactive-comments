66874c1f0ff2209a3c5d9a28fcda68b29dd3b520
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-02-14 18:16:22 -0300

    feat(hooks): cria useAppSelector

---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,9 @@
+import { RootState } from '@/store';
+
+import {
+  TypedUseSelectorHook,
+  useSelector,
+} from 'react-redux';
+
+export const useAppSelector: TypedUseSelectorHook<RootState> =
+  useSelector;
