c2dc4ce0c8fd4664adcf442310c891c3ac61a048
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-06-29 08:25:18 -0300

    test(handleVotes): cria testes para as replies

--- src/utils/tests/handleVotes.test.ts
+++ src/utils/tests/handleVotes.test.ts
@@ -50,8 +50,59 @@ describe('comments votes tests', () => {
       expectedScore: 2,
     });
   });
-
-  //Reply
-  // - realizar os mesmos testes acima
 });
 
+describe('replies votes tests', () => {
+  const parentComment = mock_comments[1];
+
+  it('should increment score and alter comment vote status to true', () => {
+    const comment = parentComment.replies[0];
+
+    incrementAndAlterVoteStatus({
+      comment: comment,
+      state: mock_comments,
+      parentId: parentComment.id,
+      expectedScore: comment.score + 1,
+    });
+  });
+
+  it('should maintain the current comment score and vote status', () => {
+    maintainScoreAndStatusVoteValue({
+      comment: mock_comments[1],
+      state: mock_comments,
+    });
+  });
+
+  it('should decrement comment votes and alter voted status to false', () => {
+    const reply = parentComment.replies[1];
+
+    decrementVotesAndAlterVoteStatus({
+      comment: reply,
+      state: mock_comments,
+      expectedScore: reply.score - 1,
+      parentId: parentComment.id,
+    });
+  });
+
+  it('should decrement only once', () => {
+    const reply = parentComment.replies[1]; 
+
+    decrementOnlyOnce({
+      comment: reply,
+      state: mock_comments,
+      expectedScore: 2,
+      parentId: parentComment.id,
+    });
+  });
+
+  it('should increment only once', () => {
+    const reply = parentComment.replies[0];
+
+    incrementOnlyOnce({
+      comment: reply,
+      state: mock_comments,
+      expectedScore: 4,
+      parentId: parentComment.id,
+    });
+  });
+});
